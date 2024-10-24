18668ef680ccbf4a8c2afeba7505adbe5a117e99
Syed Tarique Abedin <syedtarique20@gmail.com>
2024-10-24 21:50:37 +0530

    Pagicnation, filter,sorting

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 const movieRoutes = require('./router/movieRoutes')
 require('dotenv').config({path:'./config.env'})
 app.use(express.json());
-// app.get('/api/v1', (req, res) => {
-//     res.send("This is our home page");
-// })
+
 app.use('/api/v1',movieRoutes)
 mongoose.connect(process.env.DB_URL).then((conn)=>{
     console.log("Database Connected");
